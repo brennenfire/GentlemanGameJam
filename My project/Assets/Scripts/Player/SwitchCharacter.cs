cf1d6eec61428bfca70c7913a6f4c54ffa447567
brennenfire <barbustefan2002@gmail.com>
2023-12-05 18:17:31 +0200

    changes to switchcharacter for ui

--- My project/Assets/Scripts/Player/SwitchCharacter.cs
+++ My project/Assets/Scripts/Player/SwitchCharacter.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchCharacter : MonoBehaviour
 {
     [SerializeField] GameObject Graffiti;
     [SerializeField] GameObject Gentleman;
+    [SerializeField] GameObject GentlemanUI;
+    [SerializeField] GameObject GraffitiUI;
+
 
     [ContextMenu("Switch")]
     public void Switch()
@@ -21,13 +25,17 @@ public class SwitchCharacter : MonoBehaviour
         {
             SwitchObjects.Instance.Switch("Gentleman");
             Gentleman.SetActive(true);
+            GentlemanUI.SetActive(true);
             Graffiti.SetActive(false);
+            GraffitiUI.SetActive(false);
         }
         else
         {
             SwitchObjects.Instance.Switch("Graffiti");
             Gentleman.SetActive(false);
+            GentlemanUI.SetActive(false);
             Graffiti.SetActive(true);
+            GraffitiUI.SetActive(true);
         }
     }
 }
