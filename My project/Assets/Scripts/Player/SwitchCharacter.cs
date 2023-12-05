471534ef8f21b7abf5e2217b7c066bce579cd763
brennenfire <barbustefan2002@gmail.com>
2023-12-05 21:38:59 +0200

    modified switch

--- My project/Assets/Scripts/Player/SwitchCharacter.cs
+++ My project/Assets/Scripts/Player/SwitchCharacter.cs
@@ -12,6 +12,11 @@ public class SwitchCharacter : MonoBehaviour
     [SerializeField] GameObject GraffitiUI;
 
 
+    void Start()
+    {
+        Check();
+    }
+
     [ContextMenu("Switch")]
     public void Switch()
     {
@@ -21,21 +26,26 @@ public class SwitchCharacter : MonoBehaviour
     IEnumerator Wait()
     {
         yield return new WaitForSeconds(1f);
+        Check();
+    }
+
+    void Check()
+    {
         if (Graffiti.activeSelf)
         {
             SwitchObjects.Instance.Switch("Gentleman");
             Gentleman.SetActive(true);
-            GentlemanUI.SetActive(true);
+            GentlemanUI.SetActive(false);
             Graffiti.SetActive(false);
-            GraffitiUI.SetActive(false);
+            GraffitiUI.SetActive(true);
         }
         else
         {
             SwitchObjects.Instance.Switch("Graffiti");
             Gentleman.SetActive(false);
-            GentlemanUI.SetActive(false);
+            GentlemanUI.SetActive(true);
             Graffiti.SetActive(true);
-            GraffitiUI.SetActive(true);
+            GraffitiUI.SetActive(false);
         }
     }
 }
--- My project/Assets/Scripts/Scene/SwitchIndividualObject.cs
+++ My project/Assets/Scripts/Scene/SwitchIndividualObject.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class SwitchIndividualObject : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
-    Collider2D collision;
+    //SpriteRenderer spriteRenderer;
+    //Collider2D collision;
 
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        collision = GetComponent<Collider2D>();
-    }
+    //void Start()
+    //{
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //collision = GetComponent<Collider2D>();
+
+    //}
 
     public void SwitchOff()
     {
-        collision.enabled = false;
-        spriteRenderer.color = new Color(1f, 1f, 1f, 0.25f);
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.25f);
     }
     
     public void SwitchOn()
     {
-        collision.enabled = true;
-        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+        GetComponent<Collider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
     }
 }
