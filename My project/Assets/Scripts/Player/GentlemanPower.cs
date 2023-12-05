a1945b594a3e39610c65706a0b2e6d3c229b67ea
brennenfire <barbustefan2002@gmail.com>
2023-12-06 01:38:58 +0200

    updated dragging to only be vertical(very rough)

--- My project/Assets/Scripts/Player/GentlemanPower.cs
+++ My project/Assets/Scripts/Player/GentlemanPower.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class GentlemanPower : MonoBehaviour
 {
     public GameObject selectedObject;
-    Vector3 offset;
+    Vector2 offset;
     void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
             if (targetObject && targetObject.tag == "Gentleman")
             {
                 selectedObject = targetObject.transform.gameObject;
-                offset = selectedObject.transform.position - mousePosition;
+                offset = new Vector2(0, selectedObject.transform.position.y) - new Vector2(0, mousePosition.y);
             }
         }
         if (selectedObject)
         {
-            selectedObject.transform.position = mousePosition + offset;
+            selectedObject.transform.position = new Vector2(selectedObject.transform.position.x, mousePosition.y) + offset;
         }
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
