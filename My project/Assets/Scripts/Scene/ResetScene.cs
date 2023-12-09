d1978122b0d3f7dfde2a622f0ff916cbb2f19dbb
brennenfire <barbustefan2002@gmail.com>
2023-12-10 00:38:19 +0200

    reset scene sorta works still working on it

--- My project/Assets/Scripts/Scene/ResetScene.cs
+++ My project/Assets/Scripts/Scene/ResetScene.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,27 @@ public class ResetScene : MonoBehaviour
 {
     public ResetScene Instance { get; private set; }
 
+    [SerializeField] GameObject inTransition;
+    [SerializeField] GameObject outTransition;
+
     void Awake()
     {
         Instance = this;
+        inTransition.SetActive(true);
+        inTransition.GetComponent<Animator>().SetBool("Play", true);
     }
 
     [ContextMenu("Reset")]
     public void Reset()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        outTransition.SetActive(true);
+        StartCoroutine(PlayAnimations());
     }
 
+    IEnumerator PlayAnimations()
+    {
+        outTransition.GetComponent<Animator>().SetBool("Play", true);
+        yield return new WaitForSeconds(0.15f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
