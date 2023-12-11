5755f7118d0351fe84ff2ca041892a13bb65d218
brennenfire <barbustefan2002@gmail.com>
2023-12-11 22:51:02 +0200

    fixed graffiti power i think

--- My project/Assets/Scripts/Player/GraffitiPower.cs
+++ My project/Assets/Scripts/Player/GraffitiPower.cs
@@ -9,6 +9,7 @@ public class GraffitiPower : MonoBehaviour
     [SerializeField] int uses = 1;
 
     List<GameObject> graffitis = new List<GameObject>();
+    bool canPlace = true;
 
     public static GraffitiPower Instance { get; private set; }
 
@@ -32,13 +33,37 @@ public class GraffitiPower : MonoBehaviour
 
     void UsePower()
     {
-        if (graffitis.Count >= uses)
+        if (canPlace)
         {
-            Destroy(graffitis[0]);
-            graffitis.RemoveAt(0);
+            if (graffitis.Count >= uses)
+            {
+                StartCoroutine(FadeOut());
+            }
+            StartCoroutine(ResetPlacement());
+            var test = Instantiate(Graffiti, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity);
+            graffitis.Add(test);
         }
-        var test = Instantiate(Graffiti, transform.position + new Vector3(0f, 1f, 0f), Quaternion.identity);
-        graffitis.Add(test);
     }
-    
+
+    IEnumerator ResetPlacement()
+    {
+        canPlace = false;
+        yield return new WaitForSeconds(0.8f);
+        canPlace = true;
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(0.1f);
+        graffitis[0].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.8f);
+        yield return new WaitForSeconds(0.1f);
+        graffitis[0].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.6f);
+        yield return new WaitForSeconds(0.1f);
+        graffitis[0].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.4f);
+        yield return new WaitForSeconds(0.1f);
+        graffitis[0].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.2f);
+        yield return new WaitForSeconds(0.1f);
+        Destroy(graffitis[0]);
+        graffitis.RemoveAt(0);
+    }
 }
