5fb745089cf27459dcac83d75c0847f6bda34819
brennenfire <barbustefan2002@gmail.com>
2023-12-12 17:41:19 +0200

    moving the platform on the player kills the player

--- My project/Assets/Scripts/MoveablePlatform.cs
+++ My project/Assets/Scripts/MoveablePlatform.cs
@@ -43,13 +43,14 @@ public class MoveablePlatform : MonoBehaviour
         }
     }
 
-    /*
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player" && transform.position.y > collision.transform.position.y)
+        if(collision.gameObject.tag == "Player" 
+            && transform.position.y > collision.transform.position.y 
+            && DraggingPower.Instance.selectedObject != null)
         {
             ResetScene.Instance.Reset();
+            // fa un die pe player
         }
     }
-    */
 }
