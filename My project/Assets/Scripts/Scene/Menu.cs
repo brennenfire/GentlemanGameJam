d3b819345f09b917b885260b48a41287fb3248d2
brennenfire <barbustefan2002@gmail.com>
2023-12-14 16:31:10 +0200

    rudimentary implementation of menu

--- My project/Assets/Scripts/Scene/Menu.cs
+++ My project/Assets/Scripts/Scene/Menu.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] TMP_InputField input;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
-    public void LoadGame(int levelNumber)
+    public void LoadGame()
     {
-        SceneManager.LoadScene(levelNumber);
+        SceneManager.LoadScene(int.Parse(input.text));
     }
 
     public void QuitGame()
