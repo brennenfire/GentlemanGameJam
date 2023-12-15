bd4f1888224e15c9961ad943a8accbff871f7f32
brennenfire <barbustefan2002@gmail.com>
2023-12-15 18:26:19 +0200

    frog

---
+++ My project/Assets/FrogMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMove : MonoBehaviour
+{
+    [SerializeField] Vector3 targetDestination;
+
+    void Update()
+    {
+        if (transform.position != targetDestination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetDestination, 5f * Time.deltaTime);
+        }
+    }
+}
