19c6a8a9ab2652c0359eea422e7bc7fc1c9d4c13
brennenfire <barbustefan2002@gmail.com>
2023-12-16 02:07:00 +0200

    code was bugged

--- My project/Assets/Scripts/Player/ResetLists.cs
+++ My project/Assets/Scripts/Player/ResetLists.cs
@@ -7,26 +7,26 @@ public class ResetLists : MonoBehaviour
         var listPaint = PaintingPower.Instance.Paintings;
         var listGraffiti = GraffitiPower.Instance.Graffitis;
 
-        foreach (var item in listGraffiti)
-        {
-            Destroy(item);
-        }
-        foreach (var item in listPaint)
-        {
-            Destroy(item);
-        }
-        listGraffiti.RemoveAll(t => t == null);
-        listPaint.RemoveAll(t => t == null);
+        //foreach (var item in listGraffiti)
+        //{
+        //    Destroy(item);
+        //}
+        //foreach (var item in listPaint)
+        //{
+        //    Destroy(item);
+        //}
+        //listGraffiti.RemoveAll(t => t == null);
+        //listPaint.RemoveAll(t => t == null);
 
-        //for (int i = 0; i < listPaint.Count;)
-        //{
-        //    Destroy(listPaint[0]);
-        //    listPaint.RemoveAt(0);
-        //}
-        //for (int i = 0; i < listGraffiti.Count;)
-        //{
-        //    Destroy(listGraffiti[0]);
-        //    listGraffiti.RemoveAt(0);
-        //}
+        for (int i = 0; i < listPaint.Count;)
+        {
+            Destroy(listPaint[0]);
+            listPaint.RemoveAt(0);
+        }
+        for (int i = 0; i < listGraffiti.Count;)
+        {
+            Destroy(listGraffiti[0]);
+            listGraffiti.RemoveAt(0);
+        }
     }
 }
